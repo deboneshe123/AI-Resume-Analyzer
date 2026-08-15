3654275f435333fd84981583ab7efdbbca441f24
deepthi <dveepi2005@gmail.com>
2026-08-15 17:50:44 +0530

    updated login

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 export default function Login() {
   const [isLogin, setIsLogin] = useState(true);
